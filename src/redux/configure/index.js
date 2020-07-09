f1d9a83939902a06a9474563864e35b027d025be
arge01 <gevenci.arif@gmail.com>
2020-07-09 12:53:30 +0300

    build mode true

--- src/redux/configure/index.js
+++ src/redux/configure/index.js
@@ -1,13 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, /*compose*/ } from 'redux';
 import reducers from '../index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/*const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(
   applyMiddleware()
-);
+);*/
 
 const configureStore = () => {
-  return createStore(reducers, enhancer);
+  return createStore(reducers, /*enhancer*/);
 }
 
 export default configureStore;
