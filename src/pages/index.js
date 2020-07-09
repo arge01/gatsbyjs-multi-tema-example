1783fa77b5e7af944278377c1a635c78d75017ee
arge01 <gevenci.arif@gmail.com>
2020-07-09 11:12:31 +0300

    build

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Router } from "@reach/router"
 import { Row, Col, Container, ListGroup } from "react-bootstrap"
 
 import SEO from "../components/seo"
-import Layout from "../components/Layout"
 import { Provider } from "react-redux"
 import configureStore from '../redux/configure/index';
 import BlueTheme from "../themes/blue-theme"
