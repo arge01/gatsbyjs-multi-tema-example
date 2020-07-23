9567c414e9eed1299d2843ee2911ec6ef9230610
arge01 <gevenci.arif@gmail.com>
2020-07-23 22:27:58 +0300

    rooter kaldırıldı

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
 import React, { useEffect } from "react"
-import { Router } from "@reach/router"
 
 import SEO from "../components/seo"
 import BlueTheme from "../themes/blue-theme"
-import Login from "./login"
 import { graphql } from "gatsby"
-import RandomApi from "../themes/blue-theme/random-api"
 
 const IndexPage = ({data}) => {
   useEffect(() => {
@@ -16,10 +13,7 @@ const IndexPage = ({data}) => {
       <pre>{JSON.stringify(data, null, 4)}</pre>
       <SEO title="Home" keywords={[`gatsby`, `react`, `bootstrap`]} />
         <>
-          <Router>
-            <Login path="/login" />
-            <BlueTheme path="/" />
-          </Router>
+          <BlueTheme/>
         </>
     </>
   )
