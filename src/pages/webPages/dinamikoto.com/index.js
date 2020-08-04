795cd46c1d7494f9228223feb974a8f0d81f29cb
arge01 <gevenci.arif@gmail.com>
2020-08-04 21:51:54 +0300

    Add files via upload

--- src/pages/webPages/dinamikoto.com/index.js
+++ src/pages/webPages/dinamikoto.com/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import BlueTheme from '../../../themes/blue-theme'
+import { StaticQuery, graphql } from "gatsby"
 
 export default class DinamikOto extends Component {
     render() {
         return (
             <>
-              <BlueTheme/>  
+              <BlueTheme/>
+                <StaticQuery
+                  query={graphql`
+                    {
+                        allFile {
+                        edges {
+                            node {
+                            name
+                            dir
+                            }
+                        }
+                        }
+                    }
+                    `}
+                  render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+                ></StaticQuery>
             </>
         )
     }
